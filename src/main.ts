5fb3e1530cd3c5865fa6df70730a9e748ed8b141
XTZhu <a1291753307@qq.com>
2020-09-11 18:40:06 +0800

    test: no change

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,29 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as ejsMate from 'ejs-mate';
+import * as loaderConnect from 'loader-connect';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
   // 这个位置可以绑定中间件到每个路由
   // app.use(middleware)
 
-  // 获取根目录
+  // const environment = configService.get<EnvironmentConfig>('environment');
+
+  // // 获取根目录
   const rootDir = join(__dirname, '../src');
+
+  // if (isDevelopment) {
+  //   app.use(loaderConnect.less(rootDir));
+  // }
+  app.useStaticAssets(join(rootDir, 'assets'), {
+    prefix: '/assets',
+  });
   // 指定视图引擎 处理.html后缀文件
   app.engine('html', ejsMate);
   // 视图引擎
   app.set('view engine', 'html');
   // 配置模板（视图）的基本目录
-  console.log(join(rootDir, 'views'));
   app.setBaseViewsDir(join(rootDir, 'views'));
 
   await app.listen(7010);
